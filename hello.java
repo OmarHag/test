d1ab971ce7dbf36e3575225dc807096ac4f8c74d
Asad-Nur <72320078+Asad-Nur@users.noreply.github.com>
2025-04-07 14:03:28 -0700

    hopeful merge conflict

--- hello.java
+++ hello.java
@@ -1,7 +1,9 @@
 public class hello{
     public static void main(String[] args) {
-        System.out.print("Hello, World!");
-        // Asad was here
-        // I messed up the code
+        int i = 0;
+        while (i < 10) {
+            System.out.println("i = " + i);
+            i++;
+        }
     }
 }
